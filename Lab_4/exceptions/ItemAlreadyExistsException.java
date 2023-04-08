ba86ddbd9af9ea5a9c011c77e1085b1ff81e27c6
RomanKosovets <90088812+RomanKosovets@users.noreply.github.com>
2023-04-08 16:46:51 +0300

    Lab_4_fix

--- Lab_4/exceptions/ItemAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.billing.exceptions;
-
-public class ItemAlreadyExistsException extends Exception{
-    public ItemAlreadyExistsException(String message){
-        super(message);
-    }
-}
