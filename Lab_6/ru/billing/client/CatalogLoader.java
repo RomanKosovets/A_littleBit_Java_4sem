87aa307ea44ce4e66e02437da9a367240664914f
RomanKosovets <90088812+RomanKosovets@users.noreply.github.com>
2023-04-08 16:42:25 +0300

    v1.1

--- Lab_6/ru/billing/client/CatalogLoader.java
+++
@@ -1,8 +0,0 @@
-package ru.billing.client;
-import ru.billing.exceptions.CatalogLoadException;
-import ru.billing.exceptions.ItemAlreadyExistsException;
-import ru.billing.stocklist.*;
-
-public interface CatalogLoader {
-    public void load(ItemCatalog cat) throws ItemAlreadyExistsException, CatalogLoadException;
-}
